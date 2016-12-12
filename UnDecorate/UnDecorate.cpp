e5f3b774d04b24fedf1d8056db4073067e41fc54
Rozbo <75200306@qq.com>
2016-12-12 12:34:39 +0800

    init

---
+++ UnDecorate/UnDecorate.cpp
@@ -0,0 +1,22 @@
+// UnDecorate.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <windows.h> //如果不包含此头文件，编译DbgHelp.h时会产生大量语法错误
+#include <DbgHelp.h>
+#include <tchar.h>
+#include <iostream>
+#pragma comment(lib,"dbghelp.lib") //告诉链接器使用这个输入库
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	TCHAR szUndecorateName[256];
+	memset(szUndecorateName, 0, 256);
+	if (2 == argc)
+	{
+		::UnDecorateSymbolNameW(argv[1], szUndecorateName, 256, 0);
+		std::cout << szUndecorateName << std::endl;
+	}
+    return 0;
+}
+
